9fdca084468f939d704c3fd5e27ec7a9e4c2de20
Brandon <Brandon@NubCenter.attlocal.net>
2017-09-13 11:24:38 -0500

    task1: Created Phone Subclass

---
+++ src/task1/Phone.java
@@ -0,0 +1,22 @@
+
+package task1;
+
+public class Phone extends CommunicationDivice{
+    String phoneNumber;
+    
+    public Phone(String phoneNumber){
+        setPhoneNumber(phoneNumber);
+    }
+    
+    public void setPhoneNumber(String phoneNumber){
+        if(!isValidNumber(phoneNumber)) throw new IllegalArgumentException("Invalid Phone Number");
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getPhoneNumber(){return phoneNumber;}
+    
+    private boolean isValidNumber(String testNumber){
+        return testNumber != null &&
+               testNumber.matches("\\d{7-15}");
+    }
+}
