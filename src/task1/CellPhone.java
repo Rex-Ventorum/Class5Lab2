acafca2b49e62a48398ca89ebf444a1ecd2d4943
Brandon <Brandon@NubCenter.attlocal.net>
2017-09-13 11:53:09 -0500

    task1: created Updated CellPhone to have apps so it is diffrent from Generic Phone

--- src/task1/CellPhone.java
+++ src/task1/CellPhone.java
@@ -1,8 +1,31 @@
 
 package task1;
+
+import java.util.LinkedList;
+
 public class CellPhone extends Phone{
+    private static final int MAX_APP_STORAGE = 10;
+    private LinkedList<CellPhoneApp> installedApps;
     
     public CellPhone(String phoneNumber){
         super(phoneNumber);
+        installedApps = new LinkedList<>();
+    }
+    
+    public LinkedList<CellPhoneApp> getInstalledApps(){return installedApps;}
+    public void setInstalledApps(LinkedList<CellPhoneApp> installedApps){
+        if(installedApps == null || installedApps.size() > MAX_APP_STORAGE) 
+            throw new IllegalArgumentException("Installed App List may not be null and may not exciede storage limit");
+        this.installedApps = installedApps; 
+    }
+    
+    public void installApp(CellPhoneApp newApp){
+        if(newApp == null) throw new IllegalArgumentException("App May Not Be Null");
+        if(installedApps.size() >= MAX_APP_STORAGE) throw new IllegalArgumentException("Max Storage Reached no room for App");
+        installedApps.add(newApp);
+    }
+    
+    public void removeApp(CellPhoneApp deleteApp){
+        installedApps.remove(deleteApp);
     }
 }
---
+++ src/task1/CellPhoneApp.java
@@ -0,0 +1,10 @@
+
+package task1;
+
+/**
+ * Generic Empty Class Simply to make a point
+ * @author Brandon
+ */
+public class CellPhoneApp {
+    
+}
